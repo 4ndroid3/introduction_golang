3d7fc70c73052a319bcff2bb6472132c76afc747
4ndroid3 <andijaimo@gmail.com>
2023-01-20 23:57:29 -0300

    Agrega debugger,

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -18,14 +18,33 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
-// init sets initial values for variables used in the function.
-func init() { // se inicia automaticamente antes de que se inicialicen todos los campos de el modulo.
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+// Init sets initial values for variables used in the function.
+func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 // randomFormat returns one of a set of greeting messages. The returned
 // message is selected at random.
-func randomFormat() string { // cuando una funcion empieza en minusculas se indica que la funcion va a ser local. Osea no va a poder accederse desde afuera.
+func randomFormat() string {
 	// A slice of message formats.
 	formats := []string{
 		"Hi, %v. Welcome!",
@@ -33,7 +52,6 @@ func randomFormat() string { // cuando una funcion empieza en minusculas se indi
 		"Hail, %v! Well met!",
 	}
 
-	// Return a randomly selected message format by specifying
-	// a random index for the slice of formats.
+	// Return one of the message formats selected at random.
 	return formats[rand.Intn(len(formats))]
 }
